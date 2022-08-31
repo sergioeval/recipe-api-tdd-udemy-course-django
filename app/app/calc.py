fa023a6a3447d54519a6170e243deaf1043bb585
sergioeval <sergioev@gmail.com>
2022-08-30 19:16:36 -0500

    updates

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+'''
+calculator functions
+'''
+
+def add(x,y):
+    """ add x and y and return result. """
+    return x + y
+
+def subtract(x, y):
+    ''' Substract  x from y and return result '''
+    return y - x
